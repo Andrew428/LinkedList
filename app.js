2fcdcb0ba64326499fbee72f537f65205d9c5e28
avancleave <avancleave@onlinecommunityservices.com>
2019-07-12 12:41:15 -0500

    added remove dups method

--- app.js
+++ app.js
@@ -1,37 +1,18 @@
 /* LinkedList */
 import LinkedList from './util/LinkedList.js';
-import DoublyLinkedList from './util/doublyLinkedList.js';
 
 window.onload = () => {
-
-  // LinkedList
+  
   const list = new LinkedList(); 
   list.append(1);    
-  list.append(2);    
+  list.append(2); 
+  list.append(2);   
   list.append(3);
-  
+  list.append(0);
   list.prepend(0);
-  list.prepend(-1);
-
-  list.search(1);
-  list.search(3); 
-  list.search(999); 
-
-  list.deleteHead(); // -1
-  list.deleteTail(); // 3
-  list.deleteTail(); // 2
-  list.deleteHead(); // 0
-
-  list.deleteHead(); // 1
-
-  list.deleteTail();
+  list.prepend(0);  
+  console.log(list);
+  list.removeDups();
   console.log(list);
 
-    // Doubly LinkedList
-    const myDoublyLinkedList = new DoublyLinkedList(); 
-    myDoublyLinkedList.append(1);    
-    myDoublyLinkedList.append(2);    
-    myDoublyLinkedList.append(3);
-    console.log(myDoublyLinkedList);
-    
 };
--- util/LinkedList.js
+++ util/LinkedList.js
@@ -1,94 +1,162 @@
+/* Doubly Linked List */
+
 export default class LinkedList {
+  
     constructor () {
         this.head = this.tail = null;
+        this.length = 0;
     }
     // add to the end of the list / tail
   append(value) {
     // if list is empty
     if (!this.tail) {
-      this.head = this.tail = new Node(value);
+      this.head = this.tail = new Node(value)
     }
     // if linkedlist has >= one node
-    else {       
-      let oldTail = this.tail;
-      this.tail = new Node(value);
-      oldTail.next = this.tail;  
+    else {
+      let oldTail = this.tail
+      this.tail = new Node(value)
+      oldTail.next = this.tail
+      this.tail.prev = oldTail
     }
+    this.length++;
   }
 
   //add to beginning of list / head
   prepend(value) {
     // if list is empty
     if (!this.head) {
-      this.head = this.tail = new Node(value);
+      this.head = this.tail = new Node(value)
     }
     // if linkedlist has >= one node
     else {
-      let oldHead = this.head;
-      this.head = new Node(value);
-      this.head.next = oldHead;
+      let oldHead = this.head
+      this.head = new Node(value)
+      oldHead.prev = this.head
+      this.head.next = oldHead
     }
+    this.length++;
   }
 
   deleteHead() {
     // if list is empty (no head)
     if (!this.head) {
-      return null;
+      return null
     }
     // if linkedlist has >= 1 node
     else {
-      let removedHead = this.head;
+      let removedHead = this.head
       // if list has only 1 node left
       if (this.head === this.tail) {
-        this.head = this.tail = null;
+        this.head = this.tail = null
       }
       //if list has >1 node
       else {
-        this.head = this.head.next;
+        this.head = this.head.next
+        this.head.prev = null
       }
-      return removedHead.value;
+      this.length--;
+      return removedHead.value
     }
   }
 
   deleteTail() {
     // if list is empty (no tail)
     if (!this.tail) {
-      return null;
+      return null
     }
     // if linkedlist has >= one node
     else {
-      let removedTail = this.tail;
+      let removedTail = this.tail
       // if list has only 1 node left
       if (this.head === this.tail) {
         this.tail = this.head = null
       }
       //if list has >1 node
-      else {        
-        this.tail.next = null;
+      else {
+        this.tail = this.tail.prev
+        this.tail.next = null
       }
-      return removedTail.value;
+      this.length--;
+      return removedTail.value
     }
   }
 
+  deleteValue(value) {
+    console.log(value)
+    if (!this.head) {
+      return null;
+    }
+    
+    let searchNode = this.head
+    while (searchNode) {      
+      if (searchNode.value === value) { 
+        let removedValue = searchNode;
+        console.log(removedValue);
+        if(searchNode.prev){
+          searchNode.prev.next = searchNode.next;
+        }else{
+          this.head = searchNode.next;
+        }
+        if(searchNode.next){
+          searchNode.next.prev = searchNode.prev;
+        }else {
+          this.tail = searchNode.prev;
+        }
+        this.length--;        
+        return removedValue.value;
+      }
+      searchNode = searchNode.next
+    }
+        
+  }
+
   search(value) {
-    let currentNode = this.head
+    let currentNode = this.head;
 
     while (currentNode) {
       if (currentNode.value === value) {
-        return currentNode
+        return currentNode;
       }
-      currentNode = currentNode.next
+      currentNode = currentNode.next;
     }
 
     return null
-  }  
-    
+  }
+  
+  removeDups(){
+    if (!this.head || !this.head.next) {
+      console.log('No duplicates were found. Empty or a single element Linked List.');
+      return;
+    }
+
+    let n1, n2;
+    let nodes = {};
+
+    n1 = this.head;
+    n2 = n1.next;
+    nodes[n1.value] = true;
+
+    while (n2) {
+        var value = n2.value;
+        nodes[value];
+        if (nodes[value]) {
+            n1.next = n2.next;
+            this.length--;
+        } else {
+            nodes[value] = true;
+            n1 = n2;
+        }
+        n2 = n2.next;
+    }
+  }    
 }
 
 class Node {
-    constructor (value, next) {
+    constructor (value, prev, next) {
         this.value = value
-        this.next = next || null        
+        this.next = next || null
+        this.prev = prev || null
     } 
 }
 
