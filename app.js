7e306cfdd8c4ee60871c6cdd58932f82ab5fddc3
avancleave <avancleave@onlinecommunityservices.com>
2019-07-12 16:55:29 -0500

    Updated app.js

--- app.js
+++ app.js
@@ -4,15 +4,14 @@ import LinkedList from './util/LinkedList.js';
 window.onload = () => {
   
   const list = new LinkedList(); 
-  list.append(1);    
-  list.append(2); 
-  list.append(2);   
-  list.append(3);
-  list.append(0);
-  list.prepend(0);
-  list.prepend(0);  
-  console.log(list);
-  list.removeDups();
-  console.log(list);
-
+    list.append(1);    
+    list.append(2); 
+    list.append(2);   
+    list.append(3);
+    list.append(0);
+    list.prepend(0);
+    list.prepend(0);  
+    console.log(list);
+    list.removeDups();
+    console.log(list);
 };
